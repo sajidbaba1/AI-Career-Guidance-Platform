15213bb82561870baf29b79b0535609a81159af6
sajid <152436427+sajidbaba1@users.noreply.github.com>
2025-07-30 01:27:23 +0530

    Initial commit before force push

--- src/main/resources/AiCareerGuidancePlatformApplication.java
+++ src/main/resources/AiCareerGuidancePlatformApplication.java
@@ -1,9 +1,12 @@
 package com.example.aicareerguidance;
 
+import me.paulschwarz.springdotenv.DotenvPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(factory = DotenvPropertySource.class, value = "classpath:.env")
 public class AiCareerGuidancePlatformApplication {
 
     public static void main(String[] args) {
